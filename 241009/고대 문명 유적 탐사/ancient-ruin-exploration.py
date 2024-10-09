e8a97afa98570f98765eb36ae8c436b8d7295107
mipha625 <140814930+mipha625@users.noreply.github.com>
2024-10-09 19:27:52 +0900

    Title: 고대 문명 유적 탐사, Time: 240ms, Memory: 26MB, Status: Wrong Answer - CodeTree

--- 241009/고대 문명 유적 탐사/ancient-ruin-exploration.py
+++ 241009/고대 문명 유적 탐사/ancient-ruin-exploration.py
@@ -1,7 +1,4 @@
 from collections import deque
-K, M = map(int, input().split())
-arr = [list(map(int, input().split())) for _ in range(5)]
-wall = deque(map(int , input().split()))
 
 #회전 함수
 class rotate:  
@@ -69,57 +66,63 @@ class search:
                             self.cnt -= 1
         return self.total, self.temp
         
+def main():
+    K, M = map(int, input().split())
+    arr = [list(map(int, input().split())) for _ in range(5)]
+    wall = deque(map(int , input().split()))
+    rotator = rotate()     
+    searcher = search()
 
-rotate = rotate()     
-search = search()
+    while K > 0: #턴이 0보다 클 때까지
+        #탐사 진행 (회전 시도)
 
-while K > 0: #턴이 0보다 클 때까지
-    #탐사 진행 (회전 시도)
+        result = 0
+        rotate_li = []
+        total_li = []
+        temp_li = []  
 
-    result = 0
-    rotate_li = []
-    total_li = []
-    temp_li = []  
-
-    for d in range(90, 271, 90):
-        for i in range(3):
-            for j in range(3):
-                arr1 = [items[:] for items in arr]
-                rotate_li.append(rotate.rotate_degree(arr1, i, j, d))
-                
-    for i in range(27):
-        total, temp = search.find([items[:] for items in rotate_li[i]])
-        total_li.append(total)
-        temp_li.append(temp)
-        
-    max_idx = total_li.index(max(total_li))
-    max_total = total_li[max_idx]
-    
-    if max_total == 0: #발견된 유물이 없다면 다음 턴으로 이동
-        K -= 1
-        continue
-    
-    #발견된 유물이 있다면
-    result += max_total #한 턴 내에서 발굴한 유물 개수 계속 업데이트
-    max_arr = rotate_li[max_idx]
-    max_temp = sorted(temp_li[max_idx], key = lambda x: (x[1], -x[0]))
-
-    #선정된 matrix 내에서 계속 시도
-    while True:
-        arr = max_arr
-        
-        if max_total == 0: #3개 연속으로 붙어있는 유적이 없을 때까지
-            break
-
-        for i, j in max_temp:
-            new_val = wall.popleft() #새로운 값 채워넣기
-            arr[i][j] = new_val
+        for d in range(90, 271, 90):
+            for i in range(3):
+                for j in range(3):
+                    arr1 = [items[:] for items in arr]
+                    rotate_li.append(rotator.rotate_degree(arr1, i, j, d))
+                    
+        for i in range(27):
+            total, temp = searcher.find([items[:] for items in rotate_li[i]])
+            total_li.append(total)
+            temp_li.append(temp)
             
-        #유물 연쇄 획득 확인
-        max_total, max_temp = search.find([items[:] for items in arr])
-        result += max_total
-        max_temp.sort(key = lambda x: (x[1], -x[0]))
-    
-    if result != 0:       
-        print(result, end = ' ')
-    K -= 1
+        max_idx = total_li.index(max(total_li))
+        max_total = total_li[max_idx]
+        
+        if max_total == 0: #발견된 유물이 없다면 다음 턴으로 이동
+            K -= 1
+            continue
+        
+        #발견된 유물이 있다면
+        result += max_total #한 턴 내에서 발굴한 유물 개수 계속 업데이트
+        max_arr = rotate_li[max_idx]
+        max_temp = sorted(temp_li[max_idx], key = lambda x: (x[1], -x[0]))
+
+        #선정된 matrix 내에서 계속 시도
+        while True:
+            arr = max_arr
+            
+            if max_total == 0: #3개 연속으로 붙어있는 유적이 없을 때까지
+                break
+
+            for i, j in max_temp:
+                new_val = wall.popleft() #새로운 값 채워넣기
+                arr[i][j] = new_val
+                
+            #유물 연쇄 획득 확인
+            max_total, max_temp = searcher.find([items[:] for items in arr])
+            result += max_total
+            max_temp.sort(key = lambda x: (x[1], -x[0]))
+        
+        if result != 0:       
+            print(result, end = ' ')
+        K -= 1
+
+if __name__ == '__main__':
+    main()
