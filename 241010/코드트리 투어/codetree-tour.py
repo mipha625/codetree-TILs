48e6da52677578d58a15c0974d2571aa663c5d58
mipha625 <140814930+mipha625@users.noreply.github.com>
2024-10-10 14:00:53 +0900

    Title: 코드트리 투어, Time: 1571ms, Memory: 36MB, Status: Passed - CodeTree

--- 241010/코드트리 투어/codetree-tour.py
+++ 241010/코드트리 투어/codetree-tour.py
@@ -1,22 +1,23 @@
-#코드트리 랜드 건설
+from collections import deque
+import heapq
+INF = int(1e9)
+
 def c1(arr):
     n = arr.popleft()
     m = arr.popleft()
-    graph = [[] * n for _ in range(n)]
+    graph = [[] for _ in range(n)]
     for i in range(0, 3 * m, 3):
-        s = arr.popleft()
-        e = arr.popleft()
-        c = arr.popleft()
-        # s, e, c = arr[i], arr[i+1], arr[i+2]
+        s, e, c = arr[i], arr[i+1], arr[i+2]
         graph[s].append((e, c))
         if s != e:
             graph[e].append((s, c))
     return n, m, graph
 
-#최단거리 알고리즘: 다익스트라
-## 최단거리 구하는 경우: 1) 명령어 100 2) 명령어 500
 def dijkstra(start):
     global n, graph, distance
+    if distance_cache[start] is not None:
+        distance[:] = distance_cache[start][:]
+        return
     q = []
     heapq.heappush(q, (0, start))
     distance[start] = 0
@@ -29,86 +30,55 @@ def dijkstra(start):
             if cost < distance[i[0]]:
                 distance[i[0]] = cost
                 heapq.heappush(q, (cost, i[0]))
+    distance_cache[start] = distance[:]
 
-#(4) 최적의 여행 상품 판매
-class node:
-    def __init__(self, A, B, C, D):
-        self.A = A
-        self.B = B
-        self.C = C
-        self.D = D
-        
-    def __lt__(self, other):
-        if self.A > other.A:
-            return True
-        elif self.A == other.A:
-            if self.B < other.B:
-                return self.B < other.B
-            elif self.B == other.B:
-                return self.C < other.C
-            else:
-                return False
-        else:
-            return False
-    
-    def __str__(self):
-        return '{} {} {} {}'.format(self.A, self.B, self.C, self.D)
+def lazy_delete_check():
+    while tripq and deleted_trip[tripq[0][1]]:
+        heapq.heappop(tripq)
 
-
-
-from collections import deque
-import heapq
-INF = int(1e9)
 Q = int(input())
-
 commands = deque()
 tripq = []
+deleted_trip = {}
 result = []
 
+distance_cache = {}
 for _ in range(Q):
     commands = deque(map(int, input().split()))
-    com = commands.popleft() #명령어 번호 확인
-    
+    com = commands.popleft()
+
     if com == 100: #코드트리 랜드 건설
         n, m, graph = c1(commands)
-        distance = [INF] * n #다익스트라 돌릴 때마다 새로 필요
+        distance = [INF] * n
+        distance_cache = [None] * n
+        deleted_trip = {}
         dijkstra(0)
-    elif com == 200: #(2) 여행 상품 생성
+
+    elif com == 200: #여행 상품 생성
         _id, _revenue, _dest = commands
         heapq.heappush(tripq, (-_revenue + distance[_dest], _id, _revenue, distance[_dest], _dest))
-    elif com == 300: #(3) 여행 상품 취소
-        tempq = []
-        while tripq:
-            q = heapq.heappop(tripq)
-            if q[1] != commands[0]: #삭제하지 않을 id
-                heapq.heappush(tempq, q)
-        tripq = [items[:] for items in tempq] #다시 돌려놓기
-    elif com == 400: #(4) 최적의 여행 상품 판매
-        length = len(tripq)
-        tempq = []
-        while tripq:
-            q = heapq.heappop(tripq)
-            if q[3] >= INF or q[0] > 0: #출발지로부터 dest에 도달하는 것 불가 or cost > revenue여서 이득 X
-                heapq.heappush(tempq, q)
-                continue
-            else:
-                result.append(q[1])
-                break
-        if len(tempq) == length: #하나도 빠진 게 없다면
-            result.append(-1)
-            tripq = [items[:] for items in tempq]
-        else:
-            while tempq:
-                heapq.heappush(tripq, heapq.heappop(tempq))
-    elif com == 500: #(5) 여행 상품의 출발지 변경
-        distance = [INF] * n #다익스트라 돌릴 때마다 새로 필요
-        dijkstra(commands[0]) #새로운 출발지로 distance 다시 구함
-        tempq = []
-        while tripq:
-            q = heapq.heappop(tripq) #q[1]: _id, q[2]: _revenue, q[4]: _dest
-            heapq.heappush(tempq, (-q[2] + distance[q[4]], q[1], q[2], distance[q[4]], q[4]))
-        tripq = [items[:] for items in tempq]                       
+        deleted_trip[_id] = False
 
+    elif com == 300: #여행 상품 취소
+        deleted_trip[commands[0]] = True
+
+    elif com == 400: #최적의 여행 상품 판매
+        lazy_delete_check()
+        if tripq and tripq[0][3] < INF and tripq[0][0] <= 0:
+            result.append(heapq.heappop(tripq)[1])
+        else:
+            result.append(-1)
+
+    elif com == 500: #여행 상품 출발지 변경
+        new_start = commands[0]
+        distance = [INF] * n
+        dijkstra(new_start)
+        new_tripq = []
+        while tripq:
+            q = heapq.heappop(tripq)
+            if not deleted_trip[q[1]]:
+                heapq.heappush(new_tripq, (-q[2] + distance[q[4]], q[1], q[2], distance[q[4]], q[4]))
+        tripq = new_tripq
 
 for i in result:
     print(i)
